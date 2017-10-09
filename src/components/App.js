16c1be3b57ed3a544e9f92e8dc9ec89c2aac969c
herze <herzed@gmail.com>
2017-10-08 21:02:08 -0400

    Cambios De estructura del proyecto
    Se a cambiado el index de la raiz, se elimino App.js era por defecto
    Agregado un modulo raiz que unira los demas.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component{
+    render(){
+        return(
+            <div className='content'>
+                <div>Products List</div>
+                <div>Cart</div>
+            </div>
+        )
+    }
+}
